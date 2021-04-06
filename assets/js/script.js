82398ae7311f900f6880295e6b7c7354cd49f6ed
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-06 16:14:16 -0400

    trying to get slideshow to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello world! <3")
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide (n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementById("mySlides");
+    var dots = document.getElementById("dot");
+    if (n > slides.length) {slidesIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slidesIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
